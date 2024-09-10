6168316b96cfde57c3bf8d301605c5434d19f9f7
RobertAFranco <bigbobontheradio@gmail.com>
2024-09-10 16:09:31 -0400

    one to many models

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from datetime import date
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Cat(models.Model):
@@ -6,6 +13,24 @@ class Cat(models.Model):
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
     def __str__(self):
         return self.name
+    
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+	 choices=MEALS,
+	 default=MEALS[0][0]
+  )
+  # Create a cat_id FK
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
